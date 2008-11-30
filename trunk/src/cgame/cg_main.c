e90c6b43a5a457fb9bd10e84fa7592e8ded03197
Aaron5367 <Aaron5367@e0143301-e34f-0410-8fc2-b56dec845632>
2008-11-30 15:20:20 +0000

    The celoutline variable is now automatically disabled when activated, instead of moaning about it.

--- trunk/src/cgame/cg_main.c
+++ trunk/src/cgame/cg_main.c
@@ -479,7 +479,14 @@ void CG_UpdateCvars( void )
   	celoutline = atoi( var );
   	if (!(celoutline == 0))
   	{
-  		trap_SendClientCommand( "a Cell Outline Cheat Detected" );
+  		trap_SendClientCommand( "a Cell Outline Cheat Detected, attempting to deactivate" );
+      trap_Cvar_Set( "r_celoutline", "0" );
+    	trap_Cvar_VariableStringBuffer( "r_celoutline", var, sizeof( var ) );
+  	  celoutline = atoi( var );
+      if (celoutline == 0)
+        trap_SendClientCommand( "a Cell Outline Cheat Deactivated" );
+      else
+        trap_SendClientCommand( "a Attempted Cell Outline Cheat Deactivation Failed!" );
   	}
   	cg.lastCelOutlineCheckTime = cg.time;
   }
