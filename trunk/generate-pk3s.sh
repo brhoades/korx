01683100d499400fd32d9149736791ecac4500ff
Aaron5367 <Aaron5367@e0143301-e34f-0410-8fc2-b56dec845632>
2009-01-02 16:49:28 +0000

    Reverted to KoR's generate-pk3s.sh

--- trunk/generate-pk3s.sh
+++ trunk/generate-pk3s.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+cd $1
 cd data/
-num=`date +%Y%j%k%M%S`
-echo $num
-zip -9r ../korx_data_$num.pk3 * -x ".svn"
+zip -9r ../korx_data_$3.pk3 * -x ".svn"
+cd ../ui/
+zip -9r ../korx_ui_$3.pk3 * -x ".svn"
 cd ..
-zip -9r korx_ui_$num.pk3 ui scripts  -x ".svn"
-cd build/releas*/base/
-zip -9r ../../../korx_vm_$num.pk3 vm/ -x ".svn"
+zip -9r korx_vm_$3.pk3 vm/ -x ".svn"
+
