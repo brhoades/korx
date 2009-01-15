013d6334e0e92f04329ff16c836b5051884658b9
Aaron5367 <Aaron5367@e0143301-e34f-0410-8fc2-b56dec845632>
2009-01-15 23:46:30 +0000

    Put the damage back to normal on the Adv. Rant's blob, but increased the time slightly.

--- trunk/src/game/tremulous.h
+++ trunk/src/game/tremulous.h
@@ -130,10 +130,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define LEVEL4_CHARGE_REPEAT        1000
 #define LEVEL4_CHARGE_DMG           ADM(110)
 #define LEVEL4_EBLOB_THINK_TIME     5000
-#define LEVEL4_EBLOB_REPEAT         2000
+#define LEVEL4_EBLOB_REPEAT         2500
 #define LEVEL4_EBLOB_SPEED          500.0f
 #define LEVEL4_EBLOB_RANGE          200.0f
-#define LEVEL4_EBLOB_DMG            ADM(150)
+#define LEVEL4_EBLOB_DMG            ADM(200)
 
 
 
