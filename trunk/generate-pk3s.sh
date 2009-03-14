eb88004c2aaa7718140fcbb629f59474dc530a4e
Aaron5367 <Aaron5367@e0143301-e34f-0410-8fc2-b56dec845632>
2009-03-14 00:03:09 +0000

    Added 8 new emoticons of Fox's (removing the four older ones). Also made generate-pk3s.sh include the GPL, CC, and COPYING, the latter of which having been updated.

--- trunk/generate-pk3s.sh
+++ trunk/generate-pk3s.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd $1
 cd data/
-zip -9r ../korx_data_$3.pk3 * -x *.svn* *.xcf /*~ *.blend*
+zip -9r ../korx_data_$3.pk3 ../GPL ../CC ../COPYING * -x *.svn* *.xcf /*~ *.blend*
 cd ../ui/
 zip -9r ../korx_ui_$3.pk3 * -x *.svn* *.xcf /*~ *.blend*
 cd ..
