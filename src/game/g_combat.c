af854267066b447cecc9e69e8dcb688830c389d0
Aaron5367 <Aaron5367@e0143301-e34f-0410-8fc2-b56dec845632>
2009-04-14 01:43:22 +0000

    Aliens can poison each other again.

--- src/game/g_combat.c
+++ src/game/g_combat.c
@@ -1490,8 +1490,7 @@ void G_Damage( gentity_t *targ, gentity_t *inflictor, gentity_t *attacker,
     //if boosted poison every attack
     if( attacker->client && attacker->client->ps.stats[ STAT_STATE ] & SS_BOOSTED )
     {
-      if( targ->client->ps.stats[ STAT_TEAM ] == TEAM_HUMANS &&
-          mod != MOD_LEVEL2_ZAP && mod != MOD_POISON &&
+      if( mod != MOD_LEVEL2_ZAP && mod != MOD_POISON &&
           mod != MOD_LEVEL1_PCLOUD &&
           targ->client->poisonImmunityTime < level.time )
       {
