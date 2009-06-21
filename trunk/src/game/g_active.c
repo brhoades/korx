88b73c8a6ff51d0611410650aa2cb0bacd4f7c54
Aaron5367 <Aaron5367@e0143301-e34f-0410-8fc2-b56dec845632>
2009-06-21 22:23:02 +0000

    Made aids seed the RNG and converted aidsresistance to an integer for simplicity.

--- trunk/src/game/g_active.c
+++ trunk/src/game/g_active.c
@@ -1005,13 +1005,13 @@ void ClientTimerActions( gentity_t *ent, int msec )
     }
 
     //calculate resistance to infection ('aids')
-    client->pers.aidresistance = 0.0f;
+    client->pers.aidresistance = 0;
     if( BG_InventoryContainsUpgrade( UP_REGEN, client->ps.stats ) )
-      client->pers.aidresistance += 0.75f;
+      client->pers.aidresistance = 75;
     if( BG_InventoryContainsUpgrade( UP_BATTLESUIT, ent->client->ps.stats ) )
-      client->pers.aidresistance += 0.2f;
+      client->pers.aidresistance += 20;
     if( BG_InventoryContainsUpgrade( UP_HELMET, ent->client->ps.stats ) )
-      client->pers.aidresistance += 0.13f;
+      client->pers.aidresistance += 13;
 
     //infection - stay away from teh infected ones!!! :)
     if( client->infected && !OnSameTeam( client->infector , ent ) )
@@ -1028,6 +1028,7 @@ void ClientTimerActions( gentity_t *ent, int msec )
         num = trap_EntitiesInBox( mins, maxs, entityList, MAX_GENTITIES );
         for( i = 0; i < num; i++ )
         {
+          srand( level.time );
           target = &g_entities[ entityList[ i ] ];
 
           // it must a client + not infected + human + visible + not same + lives
@@ -1035,7 +1036,7 @@ void ClientTimerActions( gentity_t *ent, int msec )
               target->client->ps.stats[ STAT_TEAM ] == TEAM_HUMANS &&
               G_Visible( ent, target, CONTENTS_SOLID ) && ent != target 
               && target->health > 0 
-              && rand( ) % 100 > ( target->client->pers.aidresistance * 100 ) )
+              && ( rand( ) % 100 ) > target->client->pers.aidresistance )
           {
             target->client->infected = qtrue;
             target->client->infectionTime = level.time;
