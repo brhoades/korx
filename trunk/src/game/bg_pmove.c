33cb77678b47965da03b665604425a86efb91277
Aaron5367 <Aaron5367@e0143301-e34f-0410-8fc2-b56dec845632>
2009-07-11 17:46:26 +0000

    Fixed wallwalk on players (tested).

--- trunk/src/game/bg_pmove.c
+++ trunk/src/game/bg_pmove.c
@@ -2020,9 +2020,9 @@ static void PM_GroundClimbTrace( void )
     //if we hit something
     if( trace.fraction < 1.0f && !( trace.surfaceFlags & ( SURF_SKY | SURF_SLICK ) ) && 
 #ifdef ALIEN_WALLWALK_ENTITIES
-      i == 4 )
+      !( trace.entityNum >= MAX_GENTITIES && i != 4 ) )
 #else
-      ( trace.entityNum == ENTITYNUM_WORLD && i == 4 ) )
+      !( trace.entityNum != ENTITYNUM_WORLD && i != 4 ) )
 #endif
     {
       if( i == 2 || i == 3 )
