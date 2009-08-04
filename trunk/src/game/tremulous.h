ca20c349968ea69566cb4d74b55cb42efcf4e677
Aaron5367 <Aaron5367@e0143301-e34f-0410-8fc2-b56dec845632>
2009-08-04 19:03:33 +0000

    Takes 1.5 minutes to die in VESD unstead of 3 now.

--- trunk/src/game/tremulous.h
+++ trunk/src/game/tremulous.h
@@ -956,8 +956,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // Currently 30 seconds
 #define VAMPIRIC_ESD_DELAY          30000
 
-// Time it takes for everyone to die in VESD (3 minutes)
-#define VAMPIRIC_ESD_TTD            180000
+// Time it takes for everyone to die in VESD (1.5 minutes)
+#define VAMPIRIC_ESD_TTD            90000
 
 // Time it takes for humans to regen all their ammo (1 minute)
 #define VAMPIRIC_ESD_TAR            90000
